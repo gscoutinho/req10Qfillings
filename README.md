dc1d46fe072095aacb5f070453a92cd2a4beb138
GabrielCoutinho <eng.gabrielcoutinho@outlook.com.br>
2024-07-01 16:59:59 -0300

    stock prices to df_data and PE to df_kpi

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# req10Qfillings
-Python coding to request data from 10-Q forms for U.S stocks
+# SEC Query Data Program
+
+Este programa recupera e processa dados financeiros dos formulários 10-Q da SEC para ações especificadas. Ele calcula e visualiza principais indicadores financeiros.
+
+## Funcionalidades
+
+- Recupera dados da API da SEC para ações especificadas.
+- Processa dados financeiros dos formulários 10-Q.
+- Calcula principais indicadores financeiros.
+- Plota os indicadores financeiros para análise visual.
+- Salva os dados processados em arquivos CSV.
+
+## Requisitos
+
+- Python 3.x
+- Biblioteca Requests
+- Biblioteca Pandas
+- Biblioteca Matplotlib
+
+## Instalação
+
+1. Clone o repositório:
+    ```bash
+    git clone <repository_url>
+    ```
+
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd <project_directory>
+    ```
+
+3. Instale as bibliotecas necessárias:
+    ```bash
+    pip install requests pandas matplotlib
+    ```
+
+## Uso
+
+1. Edite a lista `stocks_watch` na função `main` para incluir os tickers das ações que você deseja analisar:
+    ```python
+    stocks_watch = ['XOM', 'CVX', 'COP', 'EOG', 'SLB']
+    ```
+
+2. Execute o programa:
+    ```bash
+    python sec_query_data.py
+    ```
+
+3. O programa irá exibir os KPIs calculados para cada ação e mostrar gráficos desses indicadores. Os dados processados serão salvos como arquivos CSV no diretório atual.
+
+## Visão Geral do Código
+
+- `main()`: A função principal que inicializa o processo para cada ação na lista `stocks_watch`. Recupera e processa dados, calcula indicadores financeiros, plota os resultados e salva os dados em arquivos CSV.
+  
+- `get_data_from_sec(ticker)`: Busca dados da API da SEC para um determinado ticker de ação.
+
+- `treat_data_from_10q(fillings_us_gaap)`: Processa os dados brutos da SEC e calcula os principais indicadores financeiros. Retorna um dicionário com os dados processados e os KPIs calculados.
+
+## Indicadores Financeiros Calculados
+
+- `current_ratio`: Ativos correntes divididos por passivos correntes.
+- `quick_ratio`: (Ativos correntes - Estoque) divididos por passivos correntes.
+- `GrossMargin`: (Receita - Custo dos Bens Vendidos) dividido por receita.
+- `OperatingMargin`: Rendimento operacional dividido por receita.
+- `roa`: Lucro líquido dividido por ativos correntes.
+- `roe`: Lucro líquido dividido pelo patrimônio líquido.
+
+## Contato
+
+Para quaisquer perguntas ou sugestões, por favor entre em contato: eng.gabrielcoutinho@outlook.com.br
+
--- query_10qform.py
+++ query_10qform.py
@@ -1,29 +1,35 @@
 import requests as req
 import pandas as pd
-import json
-from datetime import datetime
+import yfinance as yf
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import numpy as np
 
 print('Initializing SEC query data program')
 
 def main():
-    #stocks_watch = ['AAPL', 'MSFT', 'NVDA', 'AMZN', 'META','ADBE', 'GOOG', 'INTC', 'TSLA']
-    stocks_watch = ['XOM', 'CVX', 'COP', 'EOG', 'SLB']
+
+    
+    stocks_watch = ['AAPL', 'MSFT', 'NVDA', 'AMZN', 'META','ADBE', 'GOOG', 'INTC']
+    #stocks_watch = ['XOM', 'CVX', 'COP', 'EOG', 'SLB']
 
     dic_stockswatch = {}
 
     for stock in stocks_watch:
         dic_stockswatch[stock] = {
             'label': stock,
-            'df_data': treat_data_from_10q(get_data_from_sec(stock))['df_data'],
-            'df_kpi': treat_data_from_10q(get_data_from_sec(stock))['df_kpi']
+            'df_data': treat_data_from_10q(get_data_from_sec(stock), stock)['df_data'],
+            'df_kpi': treat_data_from_10q(get_data_from_sec(stock), stock)['df_kpi']
         }
+        print(dic_stockswatch[stock]['df_data'])
         print(dic_stockswatch[stock]['df_kpi'])
-        plt.figure(stock)
-        plt.plot(dic_stockswatch[stock]['df_kpi'])
-        plt.legend(dic_stockswatch[stock]['df_kpi'].columns)
-        plt.grid(True)
-        dic_stockswatch[stock]['df_data'].to_csv(stock, sep=';')
+        # plt.figure(stock)
+        # plt.plot(dic_stockswatch[stock]['df_kpi'])
+        # plt.legend(dic_stockswatch[stock]['df_kpi'].columns)
+        # plt.grid(True)
+        
+        #save to .csv
+        #dic_stockswatch[stock]['df_data'].to_csv(stock, sep=';')
     
     plt.show()
 
@@ -43,13 +49,13 @@ def get_data_from_sec(ticker):
 
     return req.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers=headers).json()['facts']['us-gaap']
 
-def treat_data_from_10q(fillings_us_gaap):
+def treat_data_from_10q(fillings_us_gaap, ticker):
     nonUSDkeys = []
     for element in fillings_us_gaap.values():
         try:
             element['units']['USD'] = [reg for reg in element['units']['USD'] if reg['form'] == '10-Q']        
             element['units']['shares'] = [reg for reg in element['units']['shares'] if reg['form'] == '10-Q']
-            
+            element['units']['USD/shares'] = [reg for reg in element['units']['USD/shares'] if reg['form'] == '10-Q']
         except:
             nonUSDkeys.append(element['label'])        
 
@@ -57,7 +63,8 @@ def treat_data_from_10q(fillings_us_gaap):
     #necessary data from 10-Q
 
     fields2find = ['AssetsCurrent', 
-                'LiabilitiesCurrent', 
+                'LiabilitiesCurrent',
+                'Liabilities',
                 'InventoryNet', 
                 'SalesRevenueNet',
                 'Revenues', 
@@ -67,6 +74,11 @@ def treat_data_from_10q(fillings_us_gaap):
                 'OperatingIncomeLoss',
                 'NetIncomeLoss',
                 'StockholdersEquity',
+                'InterestExpense',
+                'Assets',
+                'AccountsReceivableNetCurrent',
+                'CashCashEquivalentsRestrictedCashAndRestrictedCashEquivalents',
+                'EarningsPerShareBasic'
                 ]
 
     dic_10q_inputs = {}
@@ -82,8 +94,12 @@ def treat_data_from_10q(fillings_us_gaap):
             }
         try:
             dic_10q_inputs[field]['label'] = fillings_us_gaap[field]['label']
-            dic_10q_inputs[field]['rawdata']['value'] = [reg['val'] for reg in fillings_us_gaap[field]['units']['USD'] if reg['form'] == '10-Q']
-            dic_10q_inputs[field]['rawdata']['timestamp'] = [reg['end'] for reg in fillings_us_gaap[field]['units']['USD'] if reg['form'] == '10-Q']
+            if field == 'EarningsPerShareBasic':
+                dic_10q_inputs[field]['rawdata']['value'] = [reg['val'] for reg in fillings_us_gaap[field]['units']['USD/shares'] if reg['form'] == '10-Q']
+                dic_10q_inputs[field]['rawdata']['timestamp'] = [reg['end'] for reg in fillings_us_gaap[field]['units']['USD/shares'] if reg['form'] == '10-Q']
+            else:    
+                dic_10q_inputs[field]['rawdata']['value'] = [reg['val'] for reg in fillings_us_gaap[field]['units']['USD'] if reg['form'] == '10-Q']
+                dic_10q_inputs[field]['rawdata']['timestamp'] = [reg['end'] for reg in fillings_us_gaap[field]['units']['USD'] if reg['form'] == '10-Q']
             dic_10q_inputs[field]['df'] = pd.DataFrame({field: dic_10q_inputs[field]['rawdata']['value'], 'timestamp': dic_10q_inputs[field]['rawdata']['timestamp']})
             dic_10q_inputs[field]['df'] = dic_10q_inputs[field]['df'].drop_duplicates(subset=['timestamp'])
             dic_10q_inputs[field]['df'] = dic_10q_inputs[field]['df'].set_index('timestamp')
@@ -101,24 +117,43 @@ def treat_data_from_10q(fillings_us_gaap):
     for input in dic_10q_inputs.keys():
         df_10q_data = df_10q_data.merge(dic_10q_inputs[input]['df'], left_index=True, right_index= True, how='left')
         
+    
+
+    stock_prices = []
+    prices_historical_data = yf.Ticker(ticker).history(start=datetime.strptime(list(df_10q_data.index)[0], "%Y-%m-%d")-timedelta(days=10), end=datetime.strptime(list(df_10q_data.index)[-1], "%Y-%m-%d"))['Close']
+    for i in df_10q_data.index:
+            
+        #print(i, ' - ', prices_historical_data.loc[datetime.strptime(i, "%Y-%m-%d")-timedelta(days=5):datetime.strptime(i, "%Y-%m-%d")][-1])
+        try:
+            stock_prices.append(prices_historical_data.loc[datetime.strptime(i, "%Y-%m-%d")-timedelta(days=5):datetime.strptime(i, "%Y-%m-%d")][-1])
+        except:
+            stock_prices.append(np.nan)
+
+
+    df_10q_data['stock_price'] = stock_prices
 
     df_10q_data = df_10q_data.sort_index()
     df_10q_data = df_10q_data.interpolate(method='linear', axis=0)
 
+
+
     for input in df_10q_data.columns:
         df_10q_data[input] = df_10q_data[input].fillna(df_10q_data[input].rolling(window=9, min_periods=1).mean())
     
+
+    
+
 
     df_10q_kpi = pd.DataFrame(index=df_10q_data.index)
 
     try:
         df_10q_kpi['current_ratio'] = df_10q_data['AssetsCurrent']/df_10q_data['LiabilitiesCurrent']
     except:
-        df_10q_kpi['current_ratio'] = False
+        df_10q_kpi['current_ratio'] = 0
     try:
         df_10q_kpi['quick_ratio'] = (df_10q_data['AssetsCurrent'] - df_10q_data['InventoryNet'])/df_10q_data['LiabilitiesCurrent']
     except:
-        df_10q_kpi['quick_ratio'] = False
+        df_10q_kpi['quick_ratio'] = 0
 
     try:
         try:
@@ -126,7 +161,7 @@ def treat_data_from_10q(fillings_us_gaap):
         except:
             df_10q_kpi['GrossMargin'] = (df_10q_data['Revenues'] - df_10q_data['CostOfRevenue'])/df_10q_data['Revenues']
     except:
-        df_10q_kpi['GrossMargin'] = False
+        df_10q_kpi['GrossMargin'] = 0
     
     try:
         try:
@@ -134,20 +169,54 @@ def treat_data_from_10q(fillings_us_gaap):
         except:
             df_10q_kpi['OperatingMargin'] = df_10q_data['OperatingIncomeLoss']/df_10q_data['Revenues']
     except:
-        df_10q_kpi['OperatingMargin'] = False
+        df_10q_kpi['OperatingMargin'] = 0
         
 
     try:
         df_10q_kpi['roa'] = df_10q_data['NetIncomeLoss']/df_10q_data['AssetsCurrent']
     except:
-        df_10q_kpi['roa'] = False
+        df_10q_kpi['roa'] = 0
     try:
         df_10q_kpi['roe'] = df_10q_data['NetIncomeLoss']/df_10q_data['StockholdersEquity']
     except:
-        df_10q_kpi['roe'] = False
+        df_10q_kpi['roe'] = 0
+
+    try:
+        df_10q_kpi['debt_equity'] = df_10q_data['Liabilities']/df_10q_data['StockholdersEquity']
+    except:
+        df_10q_kpi['debt_equity'] = 0
+
+    try:
+        df_10q_kpi['interest_coverage'] = df_10q_data['OperatingIncomeLoss']/df_10q_data['InterestExpense']
+    except:
+        df_10q_kpi['interest_coverage'] = 0
+
+
+    try:
+        df_10q_kpi['assets_turnover'] = df_10q_data['Revenues']/df_10q_data['Assets']
+    except:
+        df_10q_kpi['assets_turnover'] = 0
+    try:
+        df_10q_kpi['receivable_turnover'] = df_10q_data['Revenues']/df_10q_data['AccountsReceivableNetCurrent']
+    except:
+        df_10q_kpi['receivable_turnover'] = 0
+    try:
+        df_10q_kpi['cash_netincome'] = df_10q_data['CashCashEquivalentsRestrictedCashAndRestrictedCashEquivalents']/df_10q_data['NetIncomeLoss']
+    except:
+        df_10q_kpi['cash_netincome'] = 0
+
+    try:
+        df_10q_kpi['eps'] = df_10q_data['EarningsPerShareBasic']
+    except:
+        df_10q_kpi['eps'] = 0
+
+    try:
+        df_10q_kpi['pe'] = df_10q_data['stock_price']/df_10q_kpi['eps']
+    except:
+        df_10q_kpi['pe'] = 0
 
     return {'df_data': df_10q_data,
-            'df_kpi': df_10q_kpi
+            'df_kpi': df_10q_kpi.dropna()
             }
 
 
