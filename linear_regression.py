e33760331920c78985c9ab5833a5e67aac334f47
GabrielCoutinho <eng.gabrielcoutinho@outlook.com.br>
2024-07-02 01:16:00 -0300

    dev for first batch of neural network

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
 import matplotlib.pyplot as plt
@@ -50,40 +51,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_
 Xkpi_train, Xkpi_test, ykpi_train, ykpi_test = train_test_split(Xkpi, ykpi, test_size=0.4, random_state=42)
 
 
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# y_train = scaler.transform(y_train)
+# X_test = scaler.transform(X_test)
 
 model= LinearRegression()
 model.fit(X_train, y_train)
 
 
-modelkpi= LinearRegression()
-modelkpi.fit(Xkpi_train, ykpi_train)
-
 #avaliacao do modelo
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 
-ykpi_pred = modelkpi.predict(Xkpi_test)
-msekpi = mean_squared_error(ykpi_test, ykpi_pred)
-
-
-
 
 print(f'Mean Squared Error: {mse}')
-print(f'Mean Squared Error: {msekpi}')
 
 plt.figure(figsize=(10, 6))
 plt.plot(y_test.index, y_test, label='y_test', marker='o')  # Plotar y_test como pontos sólidos
 plt.plot(y_test.index, y_pred, label='Prediction - 10_Q info', linestyle='--')  # Plotar y_pred como linha pontilhada
-plt.title('Stock price prediction')
-plt.xlabel('Índice')
-plt.ylabel('Valores')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-
-plt.figure(figsize=(10, 6))
-plt.plot(ykpi_test.index, ykpi_test, label='y_test', marker='o')  # Plotar y_test como pontos sólidos
-plt.plot(ykpi_test.index, ykpi_pred, label='Prediction - 10_Q KPI', linestyle='--')  # Plotar y_pred como linha pontilhada
 plt.title('Stock price prediction')
 plt.xlabel('Índice')
 plt.ylabel('Valores')
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error
+
+import matplotlib.pyplot as plt
 
 stocks_watch = ['AAPL', 'MSFT', 'NVDA', 'AMZN', 'META','ADBE', 'GOOG', 'INTC']
 
@@ -16,16 +22,53 @@ df_MSFT_kpi.fillna(df_MSFT_kpi.rolling(window=12, min_periods=3).mean(), inplace
 df_MSFT_data.fillna(df_MSFT_data.mean(), inplace=True)
 df_MSFT_kpi.fillna(df_MSFT_kpi.mean(), inplace=True)
 
-
-print(df_MSFT_data)
-
 data_correlation_matrix = df_MSFT_data.corr()
 data_relevant_features = data_correlation_matrix['stock_price'].sort_values(ascending=False).index[1:]
 #data_relevant_features = data_correlation_matrix.loc[(data_correlation_matrix['stock_price'] > 0.3) | (data_correlation_matrix['stock_price'] < -0.3)]['stock_price'].sort_values(ascending=False).index[1:]
 
-# kpi_correlation_matrix = df_MSFT_kpi.corr()
-#kpi_relevant_features = kpi_correlation_matrix['stock_price'].sort_values(ascending=False).index[1:]
-# kpi_relevant_features = kpi_correlation_matrix.loc[(kpi_correlation_matrix['stock_price'] > 0.44) | (kpi_correlation_matrix['stock_price'] < -0.44)]['stock_price'].sort_values(ascending=False).index[1:]
 
-print(data_correlation_matrix)
-# print(kpi_correlation_matrix)
+X = df_MSFT_data[data_relevant_features]
+y = df_MSFT_data['stock_price']
+
+# Dividir os dados em conjunto de treinamento, validação e teste mantendo os índices
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+
+# Padronizar os dados
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
+X_test = scaler.transform(X_test)
+
+
+# Treinar o MLPRegressor
+mlp = MLPRegressor(hidden_layer_sizes=(64, 32), max_iter=505, random_state=42)
+mlp.fit(X_train, y_train)
+
+# Fazer previsões
+y_train_pred = mlp.predict(X_train)
+y_val_pred = mlp.predict(X_val)
+y_test_pred = mlp.predict(X_test)
+
+# Avaliar o modelo
+train_mse = mean_squared_error(y_train, y_train_pred)
+val_mse = mean_squared_error(y_val, y_val_pred)
+test_mse = mean_squared_error(y_test, y_test_pred)
+
+print(f'Train MSE: {train_mse}')
+print(f'Validation MSE: {val_mse}')
+print(f'Test MSE: {test_mse}')
+
+
+
+# Plotar os resultados
+plt.figure(figsize=(14, 7))
+plt.plot(y_test.index, y_test, label='True Prices', color='blue')
+plt.plot(y_test.index, y_test_pred, label='Predicted Prices', linestyle='dotted', color='red')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.title('True vs Predicted Stock Prices')
+plt.grid()
+plt.legend()
+plt.show()
